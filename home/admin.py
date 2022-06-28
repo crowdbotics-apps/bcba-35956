fe7ce237582e897238a57144f83ca6baa824eb0d
Crowdbotics <team@crowdbotics.com>
2022-06-28 22:08:39 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Lklkkjhj
+
+admin.site.register(Lklkkjhj)
 
 # Register your models here.
--- home/models.py
+++ home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+
+
+class Lklkkjhj(models.Model):
+    "Generated Model"
+    fghgjhjg = models.BigIntegerField()
